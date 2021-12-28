282e9d427f80ba75936f3e96b4885b0662641841
Bai Tianhao <19302010001@fudan.edu.cn>
2021-12-28 17:22:31 +0800

    init

---
+++ src/main/java/com/db_project/utils/MybatisUtils.java
@@ -0,0 +1,32 @@
+package com.db_project.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 初始化sqlSessionFactory，并返回sqlSession
+ */
+public class MybatisUtils {
+    //SqlSessionFactory 对象最好是全局的单例变量
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //每个请求初始化一个SqlSession，默认为非自动提交
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
+    }
+}
